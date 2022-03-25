ec5490ab7d7525914dea275f558b74cfaa187dab
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2022-03-25 20:03:16 +0530

    GET result request

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,3 +1,6 @@
+var xml = new XMLHttpRequest;
+
+
 
 function getInp(){
     const regNo = document.querySelector('.regno').value
@@ -15,7 +18,6 @@ function getInp(){
 function onSubmit(){
     var response = getInp()
     if(response !=0 ){
-        var xml = new XMLHttpRequest;
         xml.open('POST',"http://172.16.5.94:3000/voting")
         xml.setRequestHeader('Content-Type', 'application/json')
         xml.send(JSON.stringify(response));
@@ -32,3 +34,80 @@ function onSubmit(){
     }
     
 }
+function ClubResult(){
+    var arr = []
+
+    console.log("true")
+    for(let i=0;i<15;i++) arr.push(0)
+    // arr.fill(0)
+    console.log(arr)
+    xml.open('GET',"http://172.16.5.94:3000/result/view")
+    xml.send()
+    xml.onload = function() {
+            const result = JSON.parse(this.responseText)
+            var len = result.length
+            for(let i=0;i<len;i++){
+                arr[parseInt(result[i].vote)-1]+=1
+            }
+
+            console.log(Math.max(arr))
+            let max = arr.reduce(function(a,b) {
+                return Math.max(a,b)
+            })
+            console.log()
+            const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+            datasets: [{
+                label: `Votes`,
+                data: arr,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 102, 255, 1)',
+
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    }
+}
+
