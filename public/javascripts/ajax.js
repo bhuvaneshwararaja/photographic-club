feff3f7b7c1f14e1aac21dbf08053deca02dc5f8
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2022-03-31 00:59:41 +0530

    adding Result functionalities

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,3 +1,4 @@
+
 var xml = new XMLHttpRequest;
 
 
@@ -36,80 +37,111 @@ function onSubmit(){
     .catch(error => console.error(error))
     
 }
-// function ClubResult(){
-//     var arr = []
 
-//     console.log("true")
-//     for(let i=0;i<15;i++) arr.push(0)
-//     // arr.fill(0)
-//     console.log(arr)
-//     xml.open('GET',"/result/view")
-//     xml.send()
-//     xml.onload = function() {
-//             const result = JSON.parse(this.responseText)
-//             var len = result.length
-//             for(let i=0;i<len;i++){
-//                 arr[parseInt(result[i].vote)-1]+=1
-//             }
 
-//             console.log(Math.max(arr))
-//             let max = arr.reduce(function(a,b) {
-//                 return Math.max(a,b)
-//             })
-//             console.log()
-//             const ctx = document.getElementById('myChart').getContext('2d');
-//     const myChart = new Chart(ctx, {
-//         type: 'bar',
-//         data: {
-//             labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-//             datasets: [{
-//                 label: `Votes`,
-//                 data: arr,
-//                 backgroundColor: [
-//                     'rgba(255, 99, 132, 0.2)',
-//                     'rgba(54, 162, 235, 0.2)',
-//                     'rgba(255, 206, 86, 0.2)',
-//                     'rgba(75, 192, 192, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(255, 159, 64, 0.2)'
-//                 ],
-//                 borderColor: [
-//                     'rgba(255, 99, 132, 1)',
-//                     'rgba(54, 162, 235, 1)',
-//                     'rgba(255, 206, 86, 1)',
-//                     'rgba(75, 192, 192, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(153, 102, 255, 1)',
+function CalcThreeWinner(){
+    
+    const AllPhotoVote = document.querySelectorAll('.vote--count')
+    const ResultText = document.querySelectorAll('.result--text')
+    const temp = []
+    for(let i = 0; i < AllPhotoVote.length; i++){
+        temp.push(AllPhotoVote[i].getAttribute('data-vote'))
+    }
+    temp.sort((a,b) => a-b)
+    let search = temp[temp.length-1]
+    AllPhotoVote.forEach((curr,index) => {
+        if(curr.getAttribute('data-vote') == search){
+            document.querySelector('.winner').innerHTML = `Pixie Event Winner PhotoNo ${index+1}`
+        }
+    })
+    // console.log(a)
+    let count = 1
+    var itr = 0
+    while(itr !=15){
+        if(count < 4){
+            if(AllPhotoVote[itr].getAttribute('data-vote') == search){
+                console.log(true)
 
-//                     'rgba(255, 159, 64, 1)'
-//                 ],
-//                 borderWidth: 2
-//             }]
-//         },
-//         options: {
-//             scales: {
-//                 y: {
-//                     beginAtZero: true
-//                 }
-//             }
-//         }
-//     });
+                count == 1 ? ResultText[itr].style.background ="greenyellow" :(count == 2 ? ResultText[itr].style.background ="indianred":ResultText[itr].style.background ="sandybrown")
+                count == 1 || count == 2 || count == 3 ?ResultText[itr].style.boxShadow =' rgb(255 255 255 / 64%) 0px 3px 14px 7px' :""
+                count++
+                search = temp[temp.length-count]
+                itr = 0
+            }
+        } 
+        else{
+            break
+        }
+        itr++
+    }
+    
+}
 
-//     }
-// }
+function CreateChartResult(arr){
+    const bgArray = Array(arr.length).fill('rgba(54, 162, 235, 1)')
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+            datasets: [{
+                label: `Votes`,
+                data: arr,
+                backgroundColor:bgArray,
+                borderColor:bgArray,
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function CreateNodeAndAppendResult(arr){
+    var len = arr.length
+    for(var i=0;i<len;i++){
+        const ptag = document.createElement('p')
+        const textNode1 = document.createTextNode(`photo No: ${i+1}  `)
+        ptag.appendChild(textNode1)
+        ptag.setAttribute('class','result--text')
+        const spantag = document.createElement('span')
+        const textNode2 = document.createTextNode(`Votes: ${arr[i]}`)
+        spantag.appendChild(textNode2)
+        spantag.setAttribute('class','vote--count')
+        spantag.setAttribute('data-vote',arr[i])
+
+        ptag.appendChild(spantag)
+        document.querySelector('.chart--results').appendChild(ptag)
+    }
+
+}
+
+
+
+function ClubResult(){
+    var arr = Array(15).fill(0)
+    // for(let i=0;i<15;i++) arr.push(0)
+    xml.open('GET',"/result/view")
+    xml.send()
+    xml.onload = function() {
+            const result = JSON.parse(this.responseText)
+            var len = result.length
+
+            for(let i=0;i<len;i++){
+                arr[parseInt(result[i].vote)-1]+=1
+                console.log(arr)
+            }
+           
+            CreateNodeAndAppendResult(arr)
+            CalcThreeWinner()
+            CreateChartResult(arr)
+   
+
+    }
+}
 
