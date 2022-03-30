9f7b33ba2b8257dd359405a1c5477e94241e5175
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2022-03-30 17:25:58 +0530

    adding request

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -16,98 +16,99 @@ function getInp(){
 }
 
 function onSubmit(){
-    var response = getInp()
-    if(response !=0 ){
-        xml.open('POST',"/voting")
-        xml.setRequestHeader('Content-Type', 'application/json')
-        xml.send(JSON.stringify(response));
-        xml.onload = function(){
-            if(this.responseText == "0") document.querySelector('.exist').innerHTML = "User Already Exist"
-            else{
-                document.querySelector('.regno').value = ""
-                document.querySelector('.vote').value = ""
-                document.querySelector('.th').innerHTML = "Thankyou for Voting"
-            }
-           
+    var respon = getInp()
+    fetch("/voting",{
+        method: 'POST',
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify(respon)
+    }).then((res) => {return res.json()})
+    .then((res) => { 
+        if(res.status === false){
+            document.querySelector('.exist').innerHTML = "User Already Exist"
         }
-        
-    }
+        else{
+            document.querySelector('.regno').value = ""
+            document.querySelector('.vote').value = ""
+            document.querySelector('.th').innerHTML = "Thankyou for Voting"
+        }
+    })
+    .catch(error => console.error(error))
     
 }
-function ClubResult(){
-    var arr = []
+// function ClubResult(){
+//     var arr = []
 
-    console.log("true")
-    for(let i=0;i<15;i++) arr.push(0)
-    // arr.fill(0)
-    console.log(arr)
-    xml.open('GET',"/result/view")
-    xml.send()
-    xml.onload = function() {
-            const result = JSON.parse(this.responseText)
-            var len = result.length
-            for(let i=0;i<len;i++){
-                arr[parseInt(result[i].vote)-1]+=1
-            }
+//     console.log("true")
+//     for(let i=0;i<15;i++) arr.push(0)
+//     // arr.fill(0)
+//     console.log(arr)
+//     xml.open('GET',"/result/view")
+//     xml.send()
+//     xml.onload = function() {
+//             const result = JSON.parse(this.responseText)
+//             var len = result.length
+//             for(let i=0;i<len;i++){
+//                 arr[parseInt(result[i].vote)-1]+=1
+//             }
 
-            console.log(Math.max(arr))
-            let max = arr.reduce(function(a,b) {
-                return Math.max(a,b)
-            })
-            console.log()
-            const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-            datasets: [{
-                label: `Votes`,
-                data: arr,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(153, 102, 255, 1)',
+//             console.log(Math.max(arr))
+//             let max = arr.reduce(function(a,b) {
+//                 return Math.max(a,b)
+//             })
+//             console.log()
+//             const ctx = document.getElementById('myChart').getContext('2d');
+//     const myChart = new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+//             labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+//             datasets: [{
+//                 label: `Votes`,
+//                 data: arr,
+//                 backgroundColor: [
+//                     'rgba(255, 99, 132, 0.2)',
+//                     'rgba(54, 162, 235, 0.2)',
+//                     'rgba(255, 206, 86, 0.2)',
+//                     'rgba(75, 192, 192, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(255, 159, 64, 0.2)'
+//                 ],
+//                 borderColor: [
+//                     'rgba(255, 99, 132, 1)',
+//                     'rgba(54, 162, 235, 1)',
+//                     'rgba(255, 206, 86, 1)',
+//                     'rgba(75, 192, 192, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(153, 102, 255, 1)',
 
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 2
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+//                     'rgba(255, 159, 64, 1)'
+//                 ],
+//                 borderWidth: 2
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         }
+//     });
 
-    }
-}
+//     }
+// }
 
